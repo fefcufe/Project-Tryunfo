806833eb414d4ecdf64178b4cbd4a06f1ca77fdb
Fernanda Ferreira <fefcufe@gmail.com>
2022-02-24 07:38:58 -0300

    Requisito 3 entregue

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 // abrindo PR
 class App extends React.Component {
   render() {
     return (
       <div className="main">
+
         <h1>Tryunfo</h1>
         <h2>Adicionar nova carta</h2>
+
         <div className="addCard">
           <Form />
         </div>
+
+        <div className="card">
+          <Card />
+        </div>
+
       </div>
     );
   }
